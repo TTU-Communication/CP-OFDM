a58886aee725166dda49615c74bc955533828cac
kuuhakuHorou <chang.jimmy612@gmail.com>
2024-10-17 23:02:56 +0800

    feat: Add exist files which are basic OFDM

---
+++ Companding-OFDM/equalizer.m
@@ -0,0 +1,36 @@
+function [out_sig] = equalizer(sig, ch, varargin)
+    
+    narginchk(2, 4);
+
+    validateattributes(sig, {'double'}, ...
+        {'2d', 'nonnan', 'finite', 'nonempty'}, mfilename, 'Signal');
+    validateattributes(ch, {'double'}, ...
+        {'2d', 'nonnan', 'finite', 'nonempty'}, mfilename, 'Channel');
+
+    if nargin == 2
+        mode = 1;   % Zero-Forcing
+    elseif nargin == 3
+        mode = 2;   % MMSE
+        noise_power = varargin{1};
+        validateattributes(noise_power, {'double'}, ...
+            {'2d', 'nonnan', 'finite', 'nonempty'}, mfilename, 'Noise_power');
+        signal_power = 1;
+    elseif nargin == 4
+        mode = 2;   % MMSE
+        noise_power = varargin{1};
+        validateattributes(noise_power, {'double'}, ...
+            {'2d', 'nonnan', 'finite', 'nonempty'}, mfilename, 'Noise_power');
+        signal_power = varargin{2};
+        validateattributes(signal_power, {'double'}, ...
+            {'2d', 'nonnan', 'finite', 'nonempty'}, mfilename, 'Signal_power');
+    end
+
+    switch (mode)
+        case 1  % Zero-Forcing
+            eq = 1 ./ ch;
+        case 2  % MMSE
+            eq = conj(ch) .* (ch .* conj(ch) + (noise_power ./ signal_power)) .^ -1;
+    end
+
+    out_sig = eq .* sig;
+end
