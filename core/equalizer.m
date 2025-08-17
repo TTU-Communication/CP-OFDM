af052e42b25b4d49079d40f8076c97ecefabbebf
ShiweiChang <chang.jimmy612@gmail.com>
2025-08-17 11:51:06 +0800

    feat: Enhance power handling for multi-antenna eq
    - Extended `noisePower` and `sigPower` to vary by antenna and batch.
    - Fixed performance degradation in SIMO/MIMO when `nRx > nTx`.
    - Note: MISO/MIMO with `nRx < nTx` is limited, as transmit streams still
      match `nTx`.

--- core/equalizer.m
+++ core/equalizer.m
@@ -14,22 +14,58 @@ function [outSig] = equalizer(inSig, H, noisePower, sigPower)
     inSigPage = reshape(permute(inSig, [3 1 2]), [nRX 1 sigSample  sigBatch]);
     % change dimension from [Nsp Ns Nrx Ntx] to [Nrx Ntx Nsp Ns]
     HPage = permute(H, [3 4 1 2]);
+
     if noisePower(1) ~= 0
-        noisePower = permute(repmat(noisePower, [sigSample 1 1]), [3 1 2]);
+        % change dimension from [1 Ns Nrx] to [Nrx 1 Nsp Ns]
+        noiseVar = reshape(permute(repmat(noisePower, [sigSample 1 1]), [3 1 2]), [nRX 1 sigSample sigBatch]);
         if ndims(noisePower) == 3
-            noisePower = eye(nRX) .* reshape(noisePower, [nRX 1 sigSample sigBatch]);
+            % diagonal matrices based on every column vector
+            noiseVar = eye(nRX) .* noiseVar;
         end
-    end
-    if sigPower(1) ~= 1
-        sigPower = permute(repmat(sigPower, [sigSample 1 1]), [3 1 2]);
-        if ndims(sigPower) == 3
-            sigPower = eye(nRX) .* reshape(sigPower,[nRX 1 sigSample sigBatch]);
-        end
+        % calculate the inverse matrix
+        noiseVarInv = pageinv(noiseVar);
+    else
+        % set to identity matrix for each signals
+        noiseVarInv = repmat(eye(nRX), [1 1 sigSample sigBatch]);
     end
 
-    eq = pagemrdivide(pagectranspose(HPage), pagemtimes(HPage, 'none', HPage, 'ctranspose') ...
-            + (noisePower ./ sigPower));
+    if sigPower(1) ~= 1
+        % change dimension from [1 Ns Ntx] to [Ntx 1 Nsp Ns]
+        sigVar = reshape(permute(repmat(sigPower, [sigSample 1 1]), [3 1 2]), [nTX 1 sigSample sigBatch]);
+        if ndims(sigPower) == 3
+            % diagonal matrices based on every column vector
+            sigVar = eye(nTX) .* sigVar;
+        end
+        % calculate the inverse matrix
+        sigVarInv = pageinv(sigVar);
+    elseif noiseVar(1) ~= 0
+        % set to identity matrix for each signals if noisePower is provided
+        sigVarInv = repmat(eye(nTX), [1 1 sigSample sigBatch]);
+    else
+        sigVarInv = 0;
+    end
+
+    if size(HPage, 1) >= size(HPage, 2)
+        % left inverse (generalized inverse)
+        % using $(H^H R_w^-1 H + R_x)^-1 H^H R_w^-1$
+        HPageCTNoiseVarInv = pagemtimes(HPage, 'ctranspose', noiseVarInv, 'none');
+        leftInvMtx = pagemtimes(HPageCTNoiseVarInv, HPage) + sigVarInv;
+        preEQ = pagemldivide(leftInvMtx, pagectranspose(HPage));
+        eq = pagemtimes(preEQ, noiseVarInv);
+        % eq = pagemldivide(pagemtimes(HPage, 'ctranspose', HPage, 'none') ...
+        %     + (noisePower ./ sigPower), pagectranspose(HPage));
+    else
+        % right inverse
+        % using $R_x H^H (H R_x H^H + R_w)^-1$
+        HPageSigVar = pagemtimes(HPage, sigVar);
+        rightInvMtx = pagemtimes(HPageSigVar, 'none', HPage, 'ctranspose') + noiseVar;
+        preEQ = pagemtimes(sigVar, 'none', HPage, 'ctranspose');
+        eq = pagemrdivide(preEQ, rightInvMtx);
+        % eq = pagemrdivide(pagectranspose(HPage), pagemtimes(HPage, 'none', HPage, 'ctranspose') ...
+        %     + (noisePower ./ sigPower));
+    end
 
     outSigPage = pagemtimes(eq, inSigPage);
+    % change dimension from [Ntx 1 Nsp Ns] to [Nsp Ns Ntx]
     outSig = permute(reshape(outSigPage, [nTX sigSample sigBatch]), [2 3 1]);
 end
