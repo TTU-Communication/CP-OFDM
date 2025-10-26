f1e3bb6dc10338edfe51e6315abee715f5746413
ShiweiChang <chang.jimmy612@gmail.com>
2025-10-26 21:36:08 +0800

    feat: Revise companding to use amplitude/angle
    - Switched from real/imaginary separation to amplitude/angle
      calculation.
    - Prevents uneven compression/expansion of complex signals.
    - Enhances signal consistency through the companding process.

--- Companding-OFDM/decompanding.m
+++ Companding-OFDM/decompanding.m
@@ -1,15 +1,18 @@
 function [decompanding_sig] = decompanding(sig, mu)
+
     if isreal(sig)
-        peak = max(sig);
-        decompanding_sig = sign(sig) .* peak ./ mu .* (exp(abs(sig) .* log(1 + mu) ./ peak) - 1);
+        inSig = sig;
     else
-        sig_i = real(sig);
-        sig_q = imag(sig);
-        peak_i = max(sig_i);
-        peak_q = max(sig_q);
-        decompanding_sig_i = sign(sig_i) .* peak_i ./ mu .* (exp(abs(sig_i) .* log(1 + mu) ./ peak_i) - 1);
-        decompanding_sig_q = sign(sig_q) .* peak_q ./ mu .* (exp(abs(sig_q) .* log(1 + mu) ./ peak_q) - 1);
-        decompanding_sig = decompanding_sig_i + 1i .* decompanding_sig_q;
+        inSig = abs(sig);
+    end
+
+    peak = max(inSig);
+    dSig = sign(inSig) .* peak ./ mu .* (exp(abs(inSig) .* log(1 + mu) ./ peak) - 1);
+
+    if isreal(sig)
+        decompanding_sig = dSig;
+    else
+        decompanding_sig = dSig .* exp(1j .* angle(sig));
     end
     
 end
